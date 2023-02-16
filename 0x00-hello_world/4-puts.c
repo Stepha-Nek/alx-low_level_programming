ba4fb16555bbd275409b27f4eaf98bba86df0cec
Stepha-Nek <Nneka.oguh1@gmail.com>
2023-02-16 07:44:32 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - this is the function for execution
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
