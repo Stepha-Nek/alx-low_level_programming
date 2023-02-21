d34f7727d25ea1d4b6a92f5f5291827523d9fd33
Stepha-Nek <Nneka.oguh1@gmail.com>
2023-02-21 12:54:51 -0800

    adding integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * add - adds two integers
+ *
+ * @x: first parameter to be added
+ * @y: second parameter to be added
+ * Return: Always 0 if successful
+ */
+int add(int x, int y)
+{
+	int z;
+
+	z = x + y;
+	return (z);
+
+}
+
