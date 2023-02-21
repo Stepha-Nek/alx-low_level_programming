432238c73cb50dd1f4265457805a7e3ccdf13656
Stepha-Nek <Nneka.oguh1@gmail.com>
2023-02-21 11:32:34 -0800

    Jack Bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day
+ *
+ * Return: Always 0 if successful
+ */
+void jack_bauer(void)
+{
+	int h;
+	int m;
+
+	for (h = 0; h < 24; h++)
+	{
+		for (m = 0; m < 59; m++)
+		{
+			_putchar (h / 10 + '0');
+			_putchar (h % 10 + '0');
+			_putchar (':');
+			_putchar (m / 10 + '0');
+			_putchar (m % 10 + '0');
+			_putchar ('\n');
+		}
+	}
+}
