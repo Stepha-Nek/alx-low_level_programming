a4fb76a185ba6b793ece1bd6fe601449bd929b3a
Stepha-Nek <Nneka.oguh1@gmail.com>
2023-04-10 12:14:53 -0700

    correction

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,26 +14,30 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	
 	if (fd == -1)
 	{
 		return (-1);
 	}
+	
 	if (text_content == 0)
 	{
-		text_content = " ";
+		text_content = "";
 	}
+	
 	for (num_letters = 0; text_content[num_letters]; num_letters++)
 		;
+
 	rwr = write(fd, text_content, num_letters);
 
 	if (rwr == -1)
 	{
 		return (-1);
 	}
+
 	close(fd);
 
 	return (1);
-
 }
